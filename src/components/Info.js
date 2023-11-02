63d10f865407ca8455fdcdb83a2360b598cb7606
Beko98 <nikoloz.bekoshvili25b@geolab.edu.ge>
2023-11-02 20:26:19 +0400

    needs fixing

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,46 @@
 import React from "react";
+import "../scss/_reset.scss";
+import "../scss/_info.scss";
+import { useRef, useState } from "react";
+import camera from "../images/camera.png"
 
 function Info() {
-  return <div>Info</div>;
+const inputRef = useRef(null);
+const [image, setImage] = useState("")
+
+const handleImageClick = () => {
+  inputRef.current.click();
+}
+const handleImageChange = (event) => {
+  const file = event.target.files[0]
+  setImage(event.target.files[0])
+}
+
+  return (
+    <>
+      <div className="wrapper_1">
+        <div className="card">
+          <h2>Get Started</h2>
+          <h3>add a photo</h3>
+            <div>
+                  <div className="addPhoto" onClick={handleImageClick}>
+                        {image ? (
+                        <img src={URL.createObjectURL(image)} alt="camera" className="pictureUpload" />
+                        ) : (
+                        <img src={camera} alt="camera" className="camera"/>
+                        )}
+                        <input type="file" onChange={handleImageChange} ref={inputRef}/>
+                  </div>
+            </div>
+          <p>fill in your name</p>
+          <div className="searchDiv">
+          <input type="search" placeholder="your name"/>
+          </div>
+          <button>Sign in</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Info;
